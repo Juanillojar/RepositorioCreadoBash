3f7af93c25a22ef05f0cdb367d53108d781bb3ac
Juanillojar <juanillojar@hotmail.com>
2021-11-03 19:26:48 +0100

    agrega clase Miclase.java

--- src/pruebas.java
+++ src/pruebas.java
@@ -4,6 +4,7 @@ public class pruebas {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola GitHub");
+		System.out.println("Subir el repositorio a GitHub");
 	}
 
 }
